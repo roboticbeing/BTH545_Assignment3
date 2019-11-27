2c1e97d8f77d6d3784d7441823cd0a20e3ddc4bd
WIN-V0JMUPNT1RG\Maxim <maxim124@live.com>
2019-11-26 22:42:32 -0500

    framework

--- a3/src/App.js
+++ a3/src/App.js
@@ -1,24 +1,45 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
+import './App.scss';
+
+import 'normalize.css';
+import './assets/fonts/fonts.scss';
+import './assets/stylesheets/_reset.scss';
+
+import PartOne from './components/PartOne';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+      <Router>
+        <div>
+        <div className="nav">
+        
+          <Link className="nav-item" to="/"  > 
+                <h1>Step One</h1>
+                <p>Create A Pattern By Creating Conditions And Select Files</p>
+          </Link>
+    
+          <Link className="nav-item" to="/"  > 
+                <h1>Step Two</h1>
+                <p>Select A Pattern Or Globally Target And Update The Content</p>
+          </Link>
+    
+          <Link className="nav-item" to="/"  > 
+                <h1>Step Three</h1>
+                <p>Confirmation And File Destination</p>
+          </Link>
+  
+        </div>
+          <Route
+            path='/'
+            render={() => <PartOne />}
+          />
+        </div>
+      </Router>
+    
     </div>
   );
 }
---
+++ a3/src/components/PartOne/index.js
@@ -0,0 +1,46 @@
+import React,{Component} from 'react';
+
+import './style.scss';
+
+
+class PartOne extends Component {
+    constructor(props) {
+        super(props);
+        
+        this.state = {
+
+        }
+    }
+  
+  
+
+   
+
+    componentDidMount() {
+       
+    }
+    render() {
+        return(<div>
+
+             <div className="menu-section">
+
+
+             </div>
+
+             <div className="condition-section">
+
+             </div>
+            
+            <div className="pattern-section">
+
+            </div>
+            <div className="files-section">
+
+                
+            </div>
+          
+        </div>);
+    }
+}
+
+export default PartOne
